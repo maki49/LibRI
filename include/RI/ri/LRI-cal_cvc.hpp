150b929c0e07723edb9bdf03a7f9a44d958200ea
maki49 <1579492865@qq.com>
2025-09-24 13:43:32 +0800

    fix: filter zero-tensors

--- include/RI/ri/LRI-cal_cvc.hpp
+++ include/RI/ri/LRI-cal_cvc.hpp
@@ -124,10 +124,12 @@ for K
 			{
 				if (this->filter_atom->filter_for32(Label::ab_ab::a0b0_a2b2, I, J, J))	continue; // restrict (I, J) in the irreducible sector
 				const Tensor<Tdata>& V_IJ = tools.get_Ds_ab(Label::ab::a0b0, I, J);
+				if (V_IJ.empty()) continue;
 				// symmetry: check if (I, J) in irreducible sector
 				for (TAC K : list_K)
 				{
 					const Tensor<Tdata>& C_I_IK = tools.get_Ds_ab(Label::ab::a, I, K);
+					if (C_I_IK.empty()) continue;
 					// CV_{IK,J}=C^I_{IK}V_{IJ}
 					const Tensor<Tdata> CV_IK_J = Tensor_Multiply::x1x2y1_ax1x2_ay1(C_I_IK, V_IJ);
 					for (TAC L : list_L)
@@ -136,6 +138,7 @@ for K
 						// std::array<TA, 4> key = { I, J, K, L };
 						// [CVC]_{IJKL}+=[CV]_{IK,J}C^J_{JL}	
 						const Tensor<Tdata>& C_J_JL = tools.get_Ds_ab(Label::ab::b, J, L);
+						if (C_J_JL.empty()) continue;
 						//(ika) * (ajl) = (ikjl) -> (ijkl)
 						// cvc_thread[{I, J, K, L}][R_KL] += einsum("ika, ajl -> ijkl", CV_IK_J, C_J_JL);
 						// cvc_thread[I][J][K.first][{L.first, R_KL}] += Tensor_Multiply::x0x1y1y2_x0x1a_ay1y2(CV_IK_J, C_J_JL).permute_from({ 0,2,1,3 });
@@ -155,10 +158,12 @@ for K
 			for (TAC L : list_L) // term 2
 			{
 				const Tensor<Tdata>& V_IL = tools.get_Ds_ab(Label::ab::a0b0, I, L);
+				if (V_IL.empty()) continue;
 				for (TAC K : list_K)
 				{
 					const TC& R_KL = (L.second - K.second) % period;
 					const Tensor<Tdata>& C_I_IK = tools.get_Ds_ab(Label::ab::a, I, K);
+					if (C_I_IK.empty()) continue;
 					// CV_{IK,L}: a1a2b0 = a0a1a2 * a0b0
 					const Tensor<Tdata> CV_IK_L = Tensor_Multiply::x1x2y1_ax1x2_ay1(C_I_IK, V_IL);
 					for (TAC J : list_J)
@@ -166,6 +171,7 @@ for K
 						if (this->filter_atom->filter_for32(Label::ab_ab::a0b0_a2b2, I, J, J))	continue; // restrict (I, J) in the irreducible sector
 						// [CVC]_{IJKL}+=[CV]_{IK,L}C^L_{LJ}
 						const Tensor<Tdata>& C_L_LJ = tools.get_Ds_ab(Label::ab::b, L, J);
+						if (C_L_LJ.empty()) continue;
 						// (ika) * (alj) = (iklj) -> (ijkl)
 						// cvc_thread[{I, J, K, L}][R_KL] += einsum("ika, alj -> ijkl", CV_IK_L, C_L_LJ);
 						// cvc_thread[I][J][K.first][{L.first, R_KL}] += Tensor_Multiply::x0x1y1y2_x0x1a_ay1y2(CV_IK_L, C_L_LJ).permute_from({ 0,3,1,2 });
@@ -186,10 +192,12 @@ for K
 			{
 				if (this->filter_atom->filter_for1(Label::ab_ab::a0b0_a1b2, J))	continue; // restrict J in the irreducible sector
 				const Tensor<Tdata>& V_KJ = tools.get_Ds_ab(Label::ab::a0b0, K, J);
+				if (V_KJ.empty()) continue;
 				for (TA I : list_I)
 				{
 					if (this->filter_atom->filter_for32(Label::ab_ab::a0b0_a2b2, I, J, J))	continue; // restrict (I, J) in the irreducible sector
 					const Tensor<Tdata>& C_K_KI = tools.get_Ds_ab(Label::ab::a, K, I);
+					if (C_K_KI.empty()) continue;
 					//[CV]_{KI,J}=C^K_{KI}V_{KJ}
 					const Tensor<Tdata> CV_KI_J = Tensor_Multiply::x1x2y1_ax1x2_ay1(C_K_KI, V_KJ);
 					for (TAC L : list_L)
@@ -197,6 +205,7 @@ for K
 						const TC& R_KL = (L.second - K.second) % period;
 						// [CVC]_{IJKL}+=[CV]_{KI,J}C^J_{JL}	#(3)
 						const Tensor<Tdata>& C_J_JL = tools.get_Ds_ab(Label::ab::b, J, L);
+						if (C_J_JL.empty()) continue;
 						// (kia) * (ajl) = (kijl)->(ijkl)
 						// cvc_thread[{I, J, K, L}][R_KL] += einsum("kia, ajl -> ijkl", CV_KI_J, C_J_JL);
 						// cvc_thread[I][J][K.first][{L.first, R_KL}] += Tensor_Multiply::x0x1y1y2_x0x1a_ay1y2(CV_KI_J, C_J_JL).permute_from({ 1,2,0,3 });
@@ -217,11 +226,13 @@ for K
 			for (TAC L : list_L) // term 4
 			{
 				const Tensor<Tdata>& V_KL = tools.get_Ds_ab(Label::ab::a0b0, K, L);
+				if (V_KL.empty()) continue;
 				const TC& R_KL = (L.second - K.second) % period;
 				for (TA I : list_I)
 				{
 					if (this->filter_atom->filter_for1(Label::ab_ab::a0b0_a2b2, I))	continue; // restrict I in the irreducible sector
 					const Tensor<Tdata>& C_K_KI = tools.get_Ds_ab(Label::ab::a, K, I);
+					if (C_K_KI.empty()) continue;
 					// CV_{IK,L}: a1a2b0 = a0a1a2 * a0b0
 					const Tensor<Tdata> CV_KI_L = Tensor_Multiply::x1x2y1_ax1x2_ay1(C_K_KI, V_KL);
 					for (TAC J : list_J)
@@ -229,6 +240,7 @@ for K
 						if (this->filter_atom->filter_for32(Label::ab_ab::a0b0_a2b2, I, J, J))	continue; // restrict (I, J) in the irreducible sector
 						// [CVC]_{IJKL}+=[CV]_{IK,L}C^L_{LJ}
 						const Tensor<Tdata>& C_L_LJ = tools.get_Ds_ab(Label::ab::b, L, J);
+						if (C_L_LJ.empty()) continue;
 						// (kia) * (alj) = (kilj) -> (ijkl)
 						// cvc_thread[{I, J, K, L}][R_KL] += einsum("kia, alj -> ijkl", CV_KI_L, C_L_LJ);
 						// cvc_thread[I][J][K.first][{L.first, R_KL}] += Tensor_Multiply::x0x1y1y2_x0x1a_ay1y2(CV_KI_L, C_L_LJ).permute_from({ 1,3,0,2 });
@@ -290,6 +302,7 @@ LRI<TA, Tcell, Ndim, Tdata>::constract_cvc_ds(
 					const TAC& L = map_l.first;
 					const Tensor<Tdata>& CVC_IJKL = map_l.second;
 					const Tensor<Tdata>& D_KL = tools.get_Ds_ab(Label::ab::a2b2, K, L);
+					if (D_KL.empty()) continue;
 					// Hs[I][J] += Tensor_Multiply::gemv(CVC_IJKL, D_KL)
 					LRI_Cal_Aux::add_Ds(Tensor_Multiply::gemv(CVC_IJKL, D_KL), Hs[I][J]);
 				}
